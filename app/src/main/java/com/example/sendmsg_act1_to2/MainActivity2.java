cb9a430e4cf6f144d7213685e599f23a271d6276
ayushp1 <tiwariayush7487@gmail.com>
2024-12-09 18:54:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sendmsg_act1_to2/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.sendmsg_act1_to2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        textView.setText(message);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sendMsg_act1_to2"
+include(":app")
+ 
